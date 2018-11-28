3a88a06b324b548a4f3291bbf95396e4f1869322
xiongwei <cugxy@qq.com>
2018-11-28 10:53:18 +0800

    读取图片

--- app/src/main/java/com/cugxy/imprint/AlbumReadUtil.java
+++ app/src/main/java/com/cugxy/imprint/AlbumReadUtil.java
@@ -1,5 +1,9 @@
 package com.cugxy.imprint;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
 import java.util.ArrayList;
 
 /**
@@ -10,9 +14,32 @@ import java.util.ArrayList;
 public class AlbumReadUtil {
     private final static String TGA = "AlbumReadUtil";
 
-    static public ArrayList<MarkerInfo> getAllPhotoInfo() {
+    static public ArrayList<MarkerInfo> getAllPhotoInfo(Context context) {
         ArrayList<MarkerInfo> result = new ArrayList<MarkerInfo>();
 
+        String[] projection = {
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.SIZE,
+        };
+
+        //全部图片
+        String where = MediaStore.Images.Media.MIME_TYPE + "=? or "
+                + MediaStore.Images.Media.MIME_TYPE + "=? or "
+                + MediaStore.Images.Media.MIME_TYPE + "=?";
+        //指定格式
+        String[] whereArgs = {"image/jpeg", "image/png", "image/jpg"};
+        //查询
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, where, whereArgs,
+                MediaStore.Images.Media.DATE_MODIFIED + " desc ");
+        if (cursor == null) {
+            return result;
+        }
+
+        while (cursor.moveToNext()) {
+
+        }
         return result;
     }
 
